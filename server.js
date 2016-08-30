4df6e2116a580174d2a5466099472f55a3f356cb
Brandon Hebbert <brandonhebbert@gmail.com>
2016-08-30 12:52:36 -0600

    change modal position

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-// }).listen(1337, 'localhost', function (err, result) {
-}).listen(1337, '138.68.61.64', function (err, result) {
+}).listen(1337, 'localhost', function (err, result) {
+// }).listen(1337, '138.68.61.64', function (err, result) {
   if (err) {
     console.log(err);
   }
